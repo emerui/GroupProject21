ed93fc5ff09d01d1713ac0290650f47b47bde78f
Lee Tropezado <ltropeza@charlotte.edu>
2026-04-17 00:01:04 -0400

    feat: update model_loader with new tables

---
+++ api/models/model_loader.py
@@ -0,0 +1,14 @@
+from . import orders, order_details, recipes, sandwiches, resources, customers, promotions, payments
+
+from ..dependencies.database import engine
+
+
+def index():
+    orders.Base.metadata.create_all(engine)
+    order_details.Base.metadata.create_all(engine)
+    recipes.Base.metadata.create_all(engine)
+    sandwiches.Base.metadata.create_all(engine)
+    resources.Base.metadata.create_all(engine)
+    customers.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    payments.Base.metadata.create_all(engine)
