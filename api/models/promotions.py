a76455963f9530e71d169315188330b4434249da
Lee Tropezado <ltropeza@charlotte.edu>
2026-04-16 23:58:27 -0400

    feat: add promotions model

---
+++ api/models/promotions.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DECIMAL, DATE
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+
+class Promotion(Base):
+    __tablename__ = "promotions"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(50), unique=True, nullable=False)
+    discount = Column(DECIMAL(5, 2), nullable=False, server_default='0.0')
+    expiration_date = Column(DATE, nullable=True)
+
+    orders = relationship("Order", back_populates="promotion")
