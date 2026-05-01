97fb74f88779b15dc486aaee470cc9c301b4ee50
Emely Ruiz Almaguer <eruizalm@charlotte.edu>
2026-05-01 15:45:50 -0400

    feat: basic CRUD for promotions added

--- api/models/promotions.py
+++ api/models/promotions.py
@@ -9,6 +9,7 @@ class Promotion(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(50), unique=True, nullable=False)
     discount = Column(DECIMAL(5, 2), nullable=False, server_default='0.0')
+    min_order_amount = Column(DECIMAL(6, 2), nullable=True, server_default='0.0')
     expiration_date = Column(DATE, nullable=True)
 
     orders = relationship("Order", back_populates="promotion")
